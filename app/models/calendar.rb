a32b1b160a1615743de8adf2d0d5e8835c5e870b
Gregory Hedrick <gregory@pixiitech.net>
2016-05-20 19:43:40 -0400

    Added dropboxes

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,6 +1,8 @@
 class Calendar < ActiveRecord::Base
   has_many :employees
-
+  def to_s
+  	name.to_s
+  end
   # @types = [{ :value => "icloud", :display => "iCloud"},
   # 					{ :value => "google", :display => "outlook" }]
 end
