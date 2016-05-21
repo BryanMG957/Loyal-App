4d61ae27985d3692eee64e808982977431cf5a51
Gregory Hedrick <gregory@pixiitech.net>
2016-05-21 17:11:44 -0400

    Worked on AJAX, modified bindings

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get "/auth/auth0/callback" => "auth0#callback"
   get "/auth/failure" => "auth0#failure"
 
+  get "/pull_calendars" => "calendars#pull_calendars"
+  get "/calendars/pull_calendars" => "calendars#pull_calendars"
+  
   root 'homes#index'
 
   resources :appointments
