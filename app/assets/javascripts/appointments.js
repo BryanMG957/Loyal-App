d93b4df7e2ad43d4f7f75605ef65fcbc4c1949a4
Gregory Hedrick <gregory@pixiitech.net>
2016-07-04 01:57:56 -0400

    Added JS for filling in appt values (1)

--- app/assets/javascripts/appointments.js
+++ app/assets/javascripts/appointments.js
@@ -1,20 +1,33 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
+var fillInApptData = function() {
+  var service_field = document.getElementById("appointment_service");
+  if (!service_field) return;
+  var selected_service = service_field.selectedIndex + 1;
+  $("#appointment_charge").val(
+      $("#appointment_service :nth-of-type(" + selected_service + ")").attr("data-default-price"));
+  $("#appointment_reminder_before").val(15);
+}
+
 function appointmentLoad() {
   $('.datepicker').datetimepicker({
     format: "M dd yyyy - HH:ii P",
     startView: 1,
     showMeridian: true
   });
-	$("#client-searcher").on("keyup", function() {
-		var dropdown = $("#appointment_client_id");
-		var clients = dropdown.children();
-		for ( var i = 0; i < clients.length; i++ ) {
-		  if (clients[i].innerHTML.toLowerCase().search($(this).val().toLowerCase()) > -1) {
-		  	dropdown.val(clients[i].value);
-		  	break;
-		  }
-	  }
-	});
+  $("#client-searcher").on("keyup", function() {
+	  var dropdown = $("#appointment_client_id");
+	  var clients = dropdown.children();
+	  for ( var i = 0; i < clients.length; i++ ) {
+	    if (clients[i].innerHTML.toLowerCase().search($(this).val().toLowerCase()) > -1) {
+		  dropdown.val(clients[i].value);
+		  break;
+	    }
+    }
+  });
+  $("#appointment_client_id, #appointment_service").on("change", function() {
+    fillInApptData();    
+  });
+  fillInApptData();    
 }
